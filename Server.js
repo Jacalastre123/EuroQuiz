b84287ae9ff49289344f0ce46153e6b61825364f
BrowWebs <wtran786@gmail.com>
2026-07-29 19:15:13 +1000

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,7 +3,9 @@
 async function countries() {
     loading.showModal()
     load.innerText = "Fetching Europe"
-    let res = await fetch("https://restcountries.com/v3.1/region/europe") 
+    let res = await fetch("https://restcountries.com/v5?region=europe", {headers: {
+        "Authorization": "wtran786@gmail.com rc_live_f2fc9ff67a8946de80e74fd03d3566e0"
+    }}) 
     load.innerText = "Europe Fetch Complete"
     countriesData = await res.json()
     load.innerText = "Converted Europe Data"
